5043051bb46eba0ec20683cb96eadb2400c2eed1
Chandni P <chandnirpatel@gmail.com>
2015-04-05 19:48:28 -0700

    completed refractoring for Factorial

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -2,13 +2,14 @@ package com.thoughtworks.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        int fact = 1;
+        int factorial = 1;
 
         if(i < 0) throw new IllegalArgumentException();
         if(i == 0) return 1;
         for(int j = i; j > 0; j--){
-            fact *= j;
+            factorial *= j;
         }
-        return fact;
+
+        return factorial;
     }
 }
