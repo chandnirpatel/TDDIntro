63c8faefea511bb556c91b1a20fd85868c0cd313
Chandni P <chandnirpatel@gmail.com>
2015-04-05 19:44:04 -0700

    update to pass test 5 of 5

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -2,8 +2,13 @@ package com.thoughtworks.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        int fact = 1;
+
+        if(i < 0) throw new IllegalArgumentException();
         if(i == 0) return 1;
-        if(i == 3) return 6;
-             return i;
+        for(int j = i; j > 0; j--){
+            fact *= j;
+        }
+        return fact;
     }
 }
