1f71b054cdfb2646781a314df13442d4076c07e2
Chandni P <chandnirpatel@gmail.com>
2015-04-05 21:51:59 -0700

    add test 1 for Account test and Account class

---
+++ src/test/java/com/thoughtworks/accountbalance/Account.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.accountbalance;
+
+/**
+ * Created by Chandni on 4/5/15.
+ */
+public class Account {
+    public double balance;
+
+    public Account(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public void deposit(double depositAmount) {
+        balance += depositAmount;
+    }
+}
--- src/test/java/com/thoughtworks/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/accountbalance/AccountTests.java
@@ -1,12 +1,17 @@
 package com.thoughtworks.accountbalance;
 
 import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class AccountTests {
-//    @Test
-//    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-//
-//    }
+
+    @Test
+    public void shouldIncreaseMyBalanceWhenIDepositMoney() {
+        Account account = new Account(100.0);
+        account.deposit(50.0);
+        assertThat(account.balance, is(150.0));
+    }
 //
 //    @Test
 //    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
